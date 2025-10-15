7f2c563b26dcfa5f07607939e260d200774d733f
Andrew Nakamoto <andrewsnakamoto@gmail.com>
2025-10-14 17:27:33 -0700

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className="card">
+        <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="HORIZONTAL" data-vanity="abbynakamoto" data-version="v1"></div>
+      </p>
+    </>
+  )
+}
+
+export default App
